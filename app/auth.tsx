5ab657c03a81889739ab684c76a497c01016ed48
Mohammad Anas <ansarianas53665@gmail.com>
2025-08-14 18:44:45 +0530

    Initial commit

---
+++ app/auth.tsx
@@ -0,0 +1,209 @@
+import { useState, useEffect } from "react";
+import {View,Text,StyleSheet,TouchableOpacity,Dimensions,
+Image,} from "react-native";
+import { useRouter } from "expo-router";
+import * as LocalAuthentication from "expo-local-authentication";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+
+const {width}=Dimensions.get("window");
+
+
+
+export default function AuthScreen(){
+  const [hasBiometrics,setHasBiometrics]=useState(false);
+  const [isAuthenticating,setAuthenticating] = useState(false);
+  const [error,setError]=useState<string | null>(null);
+  const router = useRouter();
+
+  useEffect(()=>{
+    checkBiometrics();
+  },[])
+
+  const checkBiometrics=async ()=>{
+  const hasHardware=await LocalAuthentication.hasHardwareAsync();
+  const isEnrolled=await LocalAuthentication.isEnrolledAsync();
+  setHasBiometrics(hasHardware && isEnrolled);
+};
+
+const authenticate = async ()=>{
+  try {
+    setAuthenticating(true);
+    setError(null);
+
+  const hasHardware = await LocalAuthentication.hasHardwareAsync();
+  const isEnrolled = await LocalAuthentication.isEnrolledAsync();
+  const supportedTypes= await LocalAuthentication.supportedAuthenticationTypesAsync();
+
+  const auth = await LocalAuthentication.authenticateAsync({
+    promptMessage: hasHardware && hasBiometrics ? 'Use face ID/TouchID' : 'Enter your PIN to access your medications',
+    fallbackLabel:'Use Pin',
+    cancelLabel:'Cancel',
+    disableDeviceFallback:false,
+  });
+
+  if(auth.success){
+    router.replace('/home')
+  }else{
+    setError('Authentication Failed : Please try again')
+  }
+  } catch (error) {
+    
+  }
+}
+
+  return(
+    <LinearGradient colors={["#3f403fff","#033b05ff"]} style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.iconContainer}>
+         <Ionicons name="medical" size={100} color='white'/>
+        </View>
+
+        <Text style={styles.title}>MEDREMIND</Text>
+
+        <Text style={styles.subtitle}>Your Personal Medication Reminder</Text>
+
+        <View style={styles.card}>
+          <Text style={styles.welcomeText}>
+            Welcome Back
+          </Text>
+          <Text style={styles.instructionsText}>
+            {hasBiometrics ? 'Use face ID/TouchID Or PIN to access your medications' : 'Enter your PIN to access your medications'}
+          </Text>
+
+          <TouchableOpacity style={[styles.button,isAuthenticating && styles.buttonDisabled]}
+          onPress={authenticate}
+          disabled={isAuthenticating}
+          >
+            <Ionicons
+            name={hasBiometrics ?
+              'finger-print-outline':'key-outline'
+            }
+            size={24}
+            color='white'
+            style={styles.buttonIcon}
+            />
+            <Text
+            style={styles.buttonText}
+            >
+              {isAuthenticating ? 'Verifying...' : hasBiometrics ? 'Authenticate' : 'Enter PIN'}
+            </Text>
+          </TouchableOpacity>
+
+          {error && (
+            <View style={styles.errorContainer}>
+            <Ionicons name="alert-circle" size={20} color="#f44336"/>
+            <Text style={styles.errorText}>{error}</Text>
+            </View>
+            )} 
+        </View>
+      </View>
+
+    </LinearGradient>
+  )
+}
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  content:{
+    flex:1,
+    padding:20,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  iconContainer:{
+    height:100,
+    backgroundColor:'rgba(255,255,255,0.3)',
+    borderRadius:50,
+    justifyContent:'center',
+    alignItems:'center',
+    marginBottom:20,
+  },
+  title:{
+    fontSize:20,
+    fontWeight:"bold",
+    color:"white",
+    marginBottom:10,
+    textShadowColor:"rgba(0,0,0,0.9)",
+    textShadowOffset:{width:1,height:1},
+    textShadowRadius:10,
+  },
+  subtitle:{
+    fontSize:18,
+    color:"rgba(255,255,255,0.9)",
+    marginBottom:40,
+    textAlign:'center',
+  },
+  card:{
+    backgroundColor:'white',
+    borderRadius:40,
+    padding:30,
+    width:width-40,
+    alignItems:'center',
+    shadowColor:'#0a0505ff',
+    shadowOffset:{
+      width:0,
+      height:5
+    },
+    shadowOpacity:0.25,
+    shadowRadius:3.84,
+    elevation:5,
+  },
+  welcomeText:{
+    fontSize:24,
+    fontWeight:"bold",
+    color:"#333",
+    marginBottom:10,
+  },
+  instructionsText:{
+     fontSize:16,
+    color:"#666",
+    marginBottom:30,
+    textAlign:"center"
+  },
+  button:{
+    backgroundColor:'#000',
+    borderRadius:26,
+    paddingVertical:15,
+    paddingHorizontal:30,
+    width:"100%",
+    alignItems:"center",
+    justifyContent:"center",
+    flexDirection:"row",
+  },
+  buttonDisabled:{
+    opacity:0.7,
+  },
+  buttonIcon:{
+    marginRight:10,
+  },
+  buttonText:{
+    color:"white",
+    fontSize:16,
+    fontWeight:"600",
+  },
+  errorContainer:{
+    flexDirection:"row",
+    alignItems:"center",
+    marginTop:10,
+    padding:10,
+    backgroundColor:"#ffebee",
+    borderRadius:8,
+  },
+  errorText:{
+    color:"#f44336",
+    fontSize:14,
+    marginLeft:8,
+  },
+
+
+})
+
+
+
+
+
+
+
